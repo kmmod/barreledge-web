18e515ead235ad23f2d49b7a08e6ed516603a4cb
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2023-03-06 23:27:09 +0100

    Dynamic light tests

--- src/stage/stage.ts
+++ src/stage/stage.ts
@@ -1,14 +1,23 @@
 import "@babylonjs/loaders";
 import "@babylonjs/core/Helpers/sceneHelpers";
-import { CreateBox, CreateGround, CreateSphere } from "@babylonjs/core/Meshes";
+import "@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent";
+import {
+  CreateBox,
+  CreateCylinder,
+  CreateGround,
+  CreateSphere,
+} from "@babylonjs/core/Meshes";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { PointLight } from "@babylonjs/core/Lights/pointLight";
 import { PhysicsImpostor } from "@babylonjs/core/Physics/v1/physicsImpostor";
 import { Scene } from "@babylonjs/core/scene";
 import { SceneLoader } from "@babylonjs/core/Loading";
-import { Vector3 } from "@babylonjs/core/Maths/math";
+import { Color3, Vector3 } from "@babylonjs/core/Maths/math";
 import { PhysicsViewer } from "@babylonjs/core/Debug/physicsViewer";
 import { Texture } from "@babylonjs/core/Materials/Textures";
+import { PBRMaterial } from "@babylonjs/core/Materials/PBR/pbrMaterial";
 import { SkyMaterial } from "@babylonjs/materials/sky";
+import { ShadowGenerator } from "@babylonjs/core/Lights/Shadows/shadowGenerator";
 
 export class Stage {
   scene: Scene;
@@ -17,6 +26,7 @@ export class Stage {
     this.scene = scene;
     this.enviroment();
     this.init();
+    this.createTestCorridorWithLights();
     // this.createTestEnvironment();
   }
 
@@ -75,6 +85,107 @@ export class Stage {
           });
       }
     );
+  }
+
+  createTestCorridorWithLights(): void {
+    const light_1 = new PointLight(
+      "light_1",
+      new Vector3(15, 4, -8),
+      this.scene
+    );
+    light_1.intensity = 100;
+    light_1.range = 20;
+    const shadowGenerator_1 = new ShadowGenerator(1024, light_1);
+
+    const light_2 = new PointLight(
+      "light_2",
+      new Vector3(20, 4, -8),
+      this.scene
+    );
+    light_2.intensity = 100;
+    light_2.range = 20;
+    const shadowGenerator_2 = new ShadowGenerator(1024, light_2);
+
+    const light_3 = new PointLight(
+      "light_3",
+      new Vector3(30, 4, -8),
+      this.scene
+    );
+    light_3.intensity = 100;
+    light_3.range = 20;
+    const shadowGenerator_3 = new ShadowGenerator(1024, light_3);
+
+    const light_4 = new PointLight(
+      "light_4",
+      new Vector3(35, 4, -8),
+      this.scene
+    );
+    light_4.intensity = 100;
+    light_4.range = 20;
+    const shadowGenerator_4 = new ShadowGenerator(1024, light_4);
+
+    const light_5 = new PointLight(
+      "light_5",
+      new Vector3(40, 4, -8),
+      this.scene
+    );
+    light_5.intensity = 100;
+    light_5.range = 20;
+    const shadowGenerator_5 = new ShadowGenerator(1024, light_5);
+
+    const material = new PBRMaterial("material", this.scene);
+    material.albedoColor = new Color3(0.6, 0.6, 0.6);
+    material.metallic = 0.0;
+    material.roughness = 0.9;
+    material.maxSimultaneousLights = 8;
+
+    const ground = CreateGround(
+      "ground1",
+      { width: 80, height: 40, subdivisions: 2 },
+      this.scene
+    );
+
+    ground.position.x = 45;
+    ground.position.y = -1;
+    ground.checkCollisions = true;
+    ground.receiveShadows = true;
+    ground.material = material;
+    shadowGenerator_1.addShadowCaster(ground, true);
+    shadowGenerator_2.addShadowCaster(ground, true);
+    shadowGenerator_3.addShadowCaster(ground, true);
+    shadowGenerator_4.addShadowCaster(ground, true);
+    shadowGenerator_5.addShadowCaster(ground, true);
+
+    for (let i = 0; i < 20; i++) {
+      const cylinder = CreateCylinder(
+        "cylinder" + i,
+        { height: 8, diameter: 1 },
+        this.scene
+      );
+      cylinder.position.x = 12 + i * 2;
+      cylinder.position.y = 3;
+      cylinder.position.z = i % 2 === 0 ? 5 : -5;
+      cylinder.checkCollisions = true;
+      cylinder.receiveShadows = true;
+      cylinder.material = material;
+      shadowGenerator_1.addShadowCaster(cylinder, true);
+      shadowGenerator_2.addShadowCaster(cylinder, true);
+      shadowGenerator_3.addShadowCaster(cylinder, true);
+      shadowGenerator_4.addShadowCaster(cylinder, true);
+      shadowGenerator_5.addShadowCaster(cylinder, true);
+    }
+    let time = 0;
+    this.scene.registerBeforeRender(() => {
+      light_1.position.z = Math.sin(time) * 8;
+      light_2.position.z = Math.sin(time + 4) * 8;
+      light_3.position.z = Math.sin(time + 8) * 8;
+      light_4.position.z = Math.sin(time + 12) * 8;
+      light_5.position.z = Math.sin(time + 16) * 8;
+
+      if (this.scene.deltaTime) {
+        time += this.scene.deltaTime / 1500;
+      }
+    });
   }
 
   createTestEnvironment(): void {
